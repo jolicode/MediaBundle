29c7e325efe9a4a2e1bfe592b490d37138eafeee
Loïc Frémont <lc.fremont@gmail.com>
2026-03-23 14:33:19 +0100

    Move javascript file into the bridge

--- src/Bridge/SyliusAdmin/config/app/twig_hooks/media.php
+++ src/Bridge/SyliusAdmin/config/app/twig_hooks/media.php
@@ -7,6 +7,12 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 return static function (ContainerConfigurator $container): void {
     $container->extension('sylius_twig_hooks', [
         'hooks' => [
+            'sylius_admin.base#javascripts' => [
+                'joli_media_sylius_admin' => [
+                    'template' => '@JoliMediaSyliusAdmin/base/scripts.html.twig',
+                ],
+            ],
+
             'joli_media_sylius_admin.media.index' => [
                 'title' => [
                     'template' => '@SyliusBootstrapAdminUi/shared/crud/common/content/header/title_block/title.html.twig',
