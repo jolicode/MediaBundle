908059541247309df501f713893c2b71c6fe2b6f
Loïc Frémont <lc.fremont@gmail.com>
2026-04-02 12:32:04 +0200

    Improve copy to clipboard UX

--- src/Bridge/SyliusAdmin/assets/js/components/clipboard.js
+++ src/Bridge/SyliusAdmin/assets/js/components/clipboard.js
@@ -5,11 +5,18 @@ const configureClipboard = () => {
         element.addEventListener('click', (event) => {
             event.preventDefault();
             const button = event.currentTarget;
+            const label = button.querySelector('.btn-label');
+            const originalText = label.textContent;
+
             navigator.clipboard.writeText(document.querySelector(button.dataset.clipboardTarget).innerText).then(() => {
-            button.classList.add('is-copied');
-            setTimeout(() => {
-                button.classList.remove('is-copied');
-            }, 2000);
+                button.classList.add('is-copied');
+                if (button.dataset.clipboardCopied) {
+                    label.textContent = button.dataset.clipboardCopied;
+                }
+                setTimeout(() => {
+                    button.classList.remove('is-copied');
+                    label.textContent = originalText;
+                }, 1000);
             });
         });
     });
