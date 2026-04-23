37bebc32dd5ec001705c254dc91d748ff0388072
Loïc Frémont <lc.fremont@gmail.com>
2026-04-23 16:30:55 +0200

    Allow to use the media on Sylius image resources

---
+++ src/Bridge/Sylius/src/Doctrine/ORM/EntityWithMediaImageTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JoliCode\MediaBundle\Bridge\Sylius\Doctrine\ORM;
+
+use Doctrine\ORM\Mapping as ORM;
+use JoliCode\MediaBundle\DeleteBehavior\Attribute\MediaDeleteBehavior;
+use JoliCode\MediaBundle\DeleteBehavior\Strategy;
+use JoliCode\MediaBundle\Doctrine\Types as MediaTypes;
+use JoliCode\MediaBundle\Model\Media;
+use JoliCode\MediaBundle\Validator\Media as MediaConstraint;
+
+trait EntityWithMediaImageTrait
+{
+    #[MediaConstraint(
+        allowedTypes: ['image'],
+        groups: ['Default', 'sylius'],
+    )]
+    #[MediaDeleteBehavior(strategy: Strategy::RESTRICT)]
+    #[ORM\Column(type: MediaTypes::MEDIA, nullable: true)]
+    private ?Media $media = null;
+
+    public function setMedia(?Media $media): void
+    {
+        if (property_exists($this, 'path')) {
+            $this->path = $media?->getPath();
+        }
+
+        $this->media = $media;
+    }
+
+    public function getMedia(): ?Media
+    {
+        return $this->media;
+    }
+}
