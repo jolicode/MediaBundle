135230e7ea6db53da2d7a7ac14c07c9254d21e36
Loïc Frémont <lc.fremont@gmail.com>
2026-07-20 12:33:40 +0200

    feat(sylius-bridge): Use new invokable grids & Grid mutators

--- src/Bridge/Sylius/config/services.php
+++ src/Bridge/Sylius/config/services.php
@@ -51,7 +51,7 @@ return static function (ContainerConfigurator $container): void {
             service('joli_media_sylius_admin.config'),
             service('translator'),
         ])
-        ->tag('sylius.grid')
+        ->tag('sylius.invokable_grid')
     ;
 
     $services->set('joli_media_sylius_admin.grid_provider.media', MediaGridProvider::class)
